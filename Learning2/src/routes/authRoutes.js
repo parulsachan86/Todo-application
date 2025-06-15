e9f73678248e3f771dd8ea29bcec1d528189b6e3
Parul Sachan <parulsachan86@gmail.com>
2025-06-15 15:38:41 +0530

    Register user API wotrking

---
+++ Learning2/src/routes/authRoutes.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
+
+const router = express.Router()
+
+// Register a new user endpoint /auth/register
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+    // save the username and an irreversibly encrypted password
+    // save gilgamesh@gmail.com | aklsdjfasdf.asdf..qwe..q.we...qwe.qw.easd
+
+    // encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
+
+    // save the new user and hashed password to the db
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        // now that we have a user, I want to add their first todo for them
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+})
+
+router.post('/login',(res, req) => {
+
+} )
+
+
+export default router;
--- Learning2/src/server.js
+++ Learning2/src/server.js
@@ -1,9 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env
+
 import express from "express";
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 // Get the file path from the URL of the current module
@@ -13,14 +18,20 @@ const __dirname = dirname(__filename)
 
 // Middleware => Use to parse json
 app.use(express.json())
+
 // Serves the HTML file from the /public directory
 // Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
 app.use(express.static(path.join(__dirname, '../public')))
+
 
 // Serving up the HTML file from the /public directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
 
 // // Adding a test route
 // app.get('/', (req, res) => {
@@ -35,3 +46,4 @@ app.listen(PORT, (err) => {
     console.log(`Server started at port ${PORT}`);
   }
 });
+
